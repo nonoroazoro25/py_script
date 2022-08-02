a18b17749a52127bf087225916070c61c6fa80b7
SZ\yansha <yansha@rayvision.com>
2022-08-02 16:25:25 +0800

    批量重命名照片名

---
+++ rename_photo.py
@@ -0,0 +1,32 @@
+import os
+
+
+class BatchRename():
+    '''
+    批量重命名文件夹中的图片文件
+    '''
+
+    def __init__(self):
+        self.path = r'C:\Users\enfuzion\Desktop\BROOKLANDS 3'  # 表示需要命名处理的文件夹
+
+    def rename(self):
+        filelist = os.listdir(self.path)  # 获取文件路径
+        total_num = len(filelist)  # 获取文件长度（个数）
+        i = 1  # 表示文件的命名是从1开始的
+        for item in filelist:
+            if item.endswith('.jpg'):  
+                src = os.path.join(os.path.abspath(self.path), item)
+                dst = os.path.join(os.path.abspath(self.path), 'DJ' + str(i) + '.jpg')  # 处理后的格式也为jpg格式的，当然这里可以改成png格式
+                # dst = os.path.join(os.path.abspath(self.path), '0000' + format(str(i), '0>3s') + '.jpg')    这种情况下的命名格式为0000000.jpg形式，可以自主定义想要的格式
+                try:
+                    os.rename(src, dst)
+                    print('converting %s to %s ...' % (src, dst))
+                    i = i + 1
+                except:
+                    continue
+        print('total %d to rename & converted %d jpgs' % (total_num, i))
+
+
+if __name__ == '__main__':
+    demo = BatchRename()
+    demo.rename()
